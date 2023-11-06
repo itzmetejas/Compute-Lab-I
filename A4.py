d6101a0dc338805dec6ddd9c0e7e0678a552d898
Tejas Shirsath <tejasshirsath111@gmail.com>
2023-11-06 23:46:00 +0530

    Add files via upload

---
+++ A4.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('Iris.csv')
+data.head()
+
+X = data.drop('Species',axis=1)
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+sse =[]
+for k in range(1,11):
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(X_scaled)
+    sse.append(kmeans.inertia_)
+    
+plt.figure(figsize=(8,6))
+plt.plot(range(1, 11), sse, marker='o')
+plt.title('Elbow Method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('SSE (Sum of Squared Distances)')
+plt.show()
+
+kmeans = KMeans(n_clusters = 3, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+y_kmeans = kmeans.fit_predict(X_scaled)
+
+plt.scatter(X_scaled[y_kmeans == 0, 0], X_scaled[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Iris-setosa')
+plt.scatter(X_scaled[y_kmeans == 1, 0], X_scaled[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Iris-versicolour')
+plt.scatter(X_scaled[y_kmeans == 2, 0], X_scaled[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Iris-virginica')
+
+
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1], s = 100, c = 'yellow', label = 'Centroids')
+
+plt.legend()
+
+
+# In[ ]:
+
+
+
+
